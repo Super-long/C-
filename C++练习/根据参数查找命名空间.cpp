4d7e1da8c007b0cbae5fddc25345213ce6461df7
zhaolonga <2339824768@qq.com>
2019-09-20 16:11:01 +0800

    namespace

--- C++练习/根据参数查找命名空间.cpp
+++ C++练习/根据参数查找命名空间.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 template<typename T>
@@ -6,6 +7,12 @@ void swap(T tmpa,T tmpb){
     using std::swap;//去掉会冲突　不会造成无限递归
     cout << "hello woeld\n";
     swap(tmpa,tmpb);
+}
+
+namespace{//相当与全局静态变量　仅本文件可见
+    string str;
+    int bookone;
+    int booktwo;
 }
 
 namespace NS{
@@ -19,6 +26,7 @@ class Bulk : public NS::Quote{
 };
 
 int main(){
+    cin >> bookone;
     string s1 = "one";
     string s2 = "two";
     swap(s1,s2);//根据参数在std命名空间中先搜寻swap函数
@@ -29,5 +37,10 @@ int main(){
     Bulk book1;
     tmpa = &book1;
     display(book1);//私有继承和保护继承基类指针不能指向派生类
+    cout << "-----------\n";
+    cin >> str;
+/*     operator>>(std::cin,booktwo);
+    operator<<(cout,bookone); *///为什么不能这样写　好奇怪
+    cout << bookone << endl;
     return 0;
 }
