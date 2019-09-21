b4fcaa3540b4594144e8fd81fcad2ef7d5b270df
zhaolonga <2339824768@qq.com>
2019-09-21 19:29:48 +0800

    虚继承

--- C++练习/虚基类.cpp
+++ C++练习/虚基类.cpp
@@ -33,11 +33,25 @@ namespace{
 }
 
 namespace VMI{
-
+    class tmpa{
+        public:
+        int tmpaa = 0;
+    };
+    class tmpb : public tmpa{
+        public:
+        int tmpbb = 1;
+    };
 }
-
+namespace hello = VMI;//hello是VMI的命名空间别名 
 int main(){
     //未命名的命名空间中
     //需要加限定符　foo cval
     //不需要限定符　bar ival
+    using VMI::tmpa;
+    using VMI::tmpb;
+    try{
+        throw VMI::tmpb();
+    }catch(tmpa &err){//就算是引用也只能使用静态绑定的类型
+        cout << err.tmpaa << endl;
+    }
 }
