f13790f831b30d0cacaed56bc747d604b32c7aea
Super-long <2339824768@qq.com>
2019-11-06 22:11:09 +0800

    water

---
+++ 内核编程/helloworld.c
@@ -0,0 +1,19 @@
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kernel.h>
+
+static int __init lkm_init(void){//编译修饰符 在模块执行完以后把内存空间回收回来
+    printk("hello world\n");
+    return 0;
+}
+
+static void __exit lkm_exit(void){
+    printk("Goodbye\n");
+}
+
+module_init(lkm_init); //引导函数 用来引导内核
+module_exit(lkm_exit);
+
+MODULE_LICENSE("GPL"); //许可证 
+
+//感觉用make是为了跨平台
--- 网络编程/ThreadPool/etest.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int (*s[10]);
-int a[10] = {1,2,3,4,4};
-
-int main(){
-    *s = 5;
-    s[5] = 10;
-    printf("%d", s[5]);
-}
