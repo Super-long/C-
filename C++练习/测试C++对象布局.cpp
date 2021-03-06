ccd3c39e7ce9c3e69d331a2648fc6bfea8566230
Super-long <2339824768@qq.com>
2021-03-06 15:04:15 +0000

    daily

---
+++ C++练习/测试C++对象布局.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+class A {
+public:
+    virtual void func_a();
+    int a = 10;
+};
+
+class D: public A {
+public:
+    virtual void func_a();
+    int d = 40;
+};
+
+void A::func_a() {std::cout << "A::func_a()" << std::endl;}
+void D::func_a() {std::cout << "D::func_a()" << std::endl;}
+
+int main() {
+    std::cout << "sizeof(A): " << sizeof(A) << std::endl;
+    std::cout << "sizeof(D): " << sizeof(D) << std::endl;
+    D* d = new D();
+    std::cout << d << std::endl;
+    A* a = d;
+    std::cout << a << std::endl;
+    d->func_a();
+    a->func_a();
+    return 0;
+}
